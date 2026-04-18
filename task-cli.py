f7aa2072501062691fb55501abf4dfccbe10312b
Thomas Fowler <tfowl95@gmail.com>
2026-04-17 21:49:10 -0500

    initial commit with basic functions

---
+++ task-cli.py
@@ -0,0 +1,29 @@
+import json
+import sys
+
+def add_task():
+    pass
+
+def update_task():
+    pass
+
+def delete_task():
+    pass
+
+def list_all_tasks():
+    pass
+
+def list_complete_tasks():
+    pass
+
+def list_incomplete_tasks():
+    pass
+
+def list_in_progress_tasks():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__task-cli__":
+    main()
